905fdc22f595ddf7fe1db909a21fface669c3c45
黄岩-Ryan <huangyan0914@sina.com>
2020-03-25 23:30:38 +0800

    存储 demo

--- src/js/Profile.js
+++ src/js/Profile.js
@@ -18,32 +18,94 @@ export default class Profile extends Component {
   	  	  return avatarFallbackImage;
   	  	},
   	  },
+
+        newStatus:"",
+        status:""
   	};
   }
 
-  render() {
-    const { handleSignOut, userSession } = this.props;
-    const { person } = this.state;
-    return (
-      !userSession.isSignInPending() ?
-      <div className="panel-welcome" id="section-2">
-        <div className="avatar-section">
-          <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
-        </div>
-        <h1>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
-          <h1> description : <span id="">{ person.description() ? person.description() : "Nothing"}</span>!</h1>
-        <p className="lead">
-          <button
-            className="btn btn-primary btn-lg"
-            id="signout-button"
-            onClick={ handleSignOut.bind(this) }
-          >
-            Logout
-          </button>
-        </p>
-      </div> : null
-    );
-  }
+    saveNewStatus(statusText) {
+        const { userSession } = this.props
+
+        let status = {
+            text: statusText.trim(),
+            created_at: Date.now()
+        }
+
+        const options = { encrypt: false }
+        userSession.putFile('status.json', JSON.stringify(status), options)
+            .then(() => {
+                this.setState({
+                    newStatus: status.text
+                })
+            })
+    }
+    fetchData() {
+        const { userSession } = this.props
+        const options = { decrypt: false }
+        userSession.getFile('status.json', options)
+            .then((file) => {
+                var status = JSON.parse(file || '[]')
+                console.log(status)
+                this.setState({
+                    status:status
+                })
+            })
+            .finally(() => {
+                console.log("read over")
+            })
+    }
+    handleNewStatusChange(event) {
+        this.setState({newStatus: event.target.value})
+    }
+
+    handleNewStatusSubmit(event) {
+        this.saveNewStatus(this.state.newStatus)
+        this.setState({
+            newStatus: ""
+        })
+    }
+
+    render() {
+        const { handleSignOut, userSession } = this.props;
+        const { person } = this.state;
+        console.log(person)
+        return (
+            !userSession.isSignInPending() ?
+                <div className="panel-welcome" id="section-2">
+                    <div className="avatar-section">
+                        <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
+                    </div>
+                    <p>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</p>
+                    <p className="lead">
+                        <button
+                            className="btn btn-primary btn-lg"
+                            id="signout-button"
+                            onClick={ handleSignOut.bind(this) }
+                        >
+                            Logout
+                        </button>
+                    </p>
+                    <br/>
+                    <br/>
+                    <textarea className="input-status"
+                              value={this.state.newStatus}
+                              onChange={e => this.handleNewStatusChange(e)}
+                              placeholder="输入状态"
+                    />
+                    <br/>
+
+                    <button
+                        className="btn btn-primary btn-lg"
+                        onClick={e => this.handleNewStatusSubmit(e)}
+                    >
+                        提交
+                    </button>
+                    <p>  status is: {this.state.status.text}</p>
+
+                </div> : null
+        );
+    }
 
   componentWillMount() {
     const { userSession } = this.props;
@@ -51,4 +113,9 @@ export default class Profile extends Component {
       person: new Person(userSession.loadUserData().profile),
     });
   }
+
+    componentDidMount() {
+        this.fetchData()
+    }
+
 }
